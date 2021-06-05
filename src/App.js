36779a8a5b628c5cd9e5e349a246351041cea30d
Mekan89 <40357987+Mekan89@users.noreply.github.com>
2021-06-05 18:18:16 +0500

    added sort feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { useState } from "react";
 import people from "./people.json";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsArrowDownShort, BsArrowUpShort } from "react-icons/bs";
 import { Table, Thead, Tbody, Tr, Th, Td, Text, Input, InputGroup, InputLeftElement, Box } from "@chakra-ui/react";
-import { Checkbox, Stack } from "@chakra-ui/react";
-import { v4 as uuidv4 } from 'uuid';
+import { Checkbox, Stack, Flex, Icon } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
+import useSortableData from './useSortableData'
+
 
 function App() {
   const data = people;
   const [q, setQ] = useState("");
   const [searchColumn, setSearchColumn] = useState(["first_name", "last_name"]);
+  // const [direction, setDirection] = useState("asc");
+  // const [value, setValue] = useState("first_name");
+  const { items, requestSort, sortConfig } = useSortableData(data);
 
   const headers = Object.keys(data[0]).splice(0, 4);
+
+  //  Sort step 1
+  // const orderBy = (data, direction, value) => {
+  //   if (direction === "desc") {
+  //     return [...data].sort((a, b) => (a[value] > b[value] ? 1 : -1));
+  //   }
+  //   if (direction === "asc") {
+  //     return [...data].sort((a, b) => (a[value] > b[value] ? -1 : 1));
+  //   }
+
+  //   return data;
+  // };
+
+  //  Sort step 2
+  const SortArrow = direction => {
+    if (direction === "desc") {
+      return <Icon as={BsArrowDownShort} w={5} h={5} />;
+    } else if (direction === "asc") {
+      return <Icon as={BsArrowUpShort} w={5} h={5} />; }
+    // } else {
+    //   setDirection(null);
+    // }
+  };
+
+  //  Sort step 3
+  // const orderedCountries = orderBy(data, direction, value);
+
+  // const switchDirection = () => {
+  //   direction === "desc" ? setDirection("asc") : setDirection("desc");
+  // };
+
+  // const setValueAndDirection = value => {
+  //   switchDirection();
+  //   setValue(value);
+  // };
 
   const search = rows => {
     return rows.filter(row =>
@@ -18,22 +58,22 @@ function App() {
     );
   };
 
-const searchHandler = (e) => {
-  const {value} = e.target;
-  const inState = [...searchColumn];
-  const foundItem = inState.indexOf(value);
+  const searchHandler = e => {
+    const { value } = e.target;
+    const inState = [...searchColumn];
+    const foundItem = inState.indexOf(value);
 
-  if(foundItem < 0) {
-    inState.push(value)
-  } else {
-    inState.splice(foundItem, 1)
-  }
+    if (foundItem < 0) {
+      inState.push(value);
+    } else {
+      inState.splice(foundItem, 1);
+    }
 
-  setSearchColumn(inState)
-}
+    setSearchColumn(inState);
+  };
 
   return (
-    <Box>
+    <Box w="100%">
       <Box textAlign="center" p="3" bg="gray" color="white">
         <Text fontSize="3xl" p="3">
           Data Filter & Sort
@@ -41,29 +81,33 @@ const searchHandler = (e) => {
       </Box>
       <Box>
         <InputGroup>
-          <InputLeftElement children={<BsSearch />} />
+          <InputLeftElement children={<Icon as={BsSearch} />} />
           <Input placeholder="Search" value={q} onChange={e => setQ(e.target.value)} />
         </InputGroup>
       </Box>
-      <Box p='3'>
+      <Flex p="3">
         <Stack spacing={10} direction="row">
           {headers.map(item => (
-            <Checkbox key={uuidv4()} value={item} isChecked={searchColumn.includes(item)} onChange={searchHandler}>{item}</Checkbox>
+            <Checkbox key={uuidv4()} value={item} isChecked={searchColumn.includes(item)} onChange={searchHandler}>
+              {item}
+            </Checkbox>
           ))}
         </Stack>
-      </Box>
+      </Flex>
 
       <Box overflowX="auto">
         <Table variant="striped" colorScheme="gray" size="sm">
           <Thead>
             <Tr>
               {headers.map(item => (
-                <Th key={uuidv4()}>{item}</Th>
+                <Th key={uuidv4()} onClick={() => requestSort(headers.filter(el => el === item).toString()) } p="1" w="25%">
+                  {item}
+                </Th>
               ))}
             </Tr>
           </Thead>
           <Tbody>
-            {search(data).map(row => (
+            {search(items).map(row => (
               <Tr key={uuidv4()}>
                 {headers.map(column => (
                   <Td key={uuidv4()}>{row[column]}</Td>
---
+++ src/useSortableData.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+
+const useSortableData = (items, config = null) => {
+    const [sortConfig, setSortConfig] = React.useState(config);
+    
+    const sortedItems = React.useMemo(() => {
+      let sortableItems = [...items];
+      if (sortConfig !== null) {
+        sortableItems.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'asc' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'asc' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return sortableItems;
+    }, [items, sortConfig]);
+  
+    const requestSort = key => {
+      let direction = 'asc';
+      if (sortConfig && sortConfig.key === key && sortConfig.direction === 'asc') {
+        direction = 'desc';
+      }
+      setSortConfig({ key, direction });
+    }
+  
+    return { items: sortedItems, requestSort };
+  }
+
+  export default useSortableData;
